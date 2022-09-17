995020d560793570f2581e59dd22a9063c4fc58d
GaryDavis <davisga1999@gmail.com>
2022-09-17 07:07:19 -0500

    commit tree d6a20a6b347a313f706377f1a037ca43ddfb2bfa

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards() {
+    const thanks = []
+    for (let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`); 
+    }
+return thanks
+}
+function countDown(x) {
+    let n = x;
+    while (n > -1) {
+        console.log(n);
+        n--;
+    }
+}
